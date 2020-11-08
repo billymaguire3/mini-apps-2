db2e15ccb99ddeb0a34f44569e2000a4a2523492
William Maguire <billymaguire3@gmail.com>
2020-11-07 19:35:50 -0500

    Add scores array to BowlingNumbers state to keep track scores posted so far

--- challenge_3/client/components/BowlingNumbers.jsx
+++ challenge_3/client/components/BowlingNumbers.jsx
@@ -7,14 +7,24 @@ class BowlingNumbers extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rollValue: ''
+      rollValue: '',
+      scores: [],
     };
     this.handlePinsEntryChange = this.handlePinsEntryChange.bind(this);
+    this.addScores = this.addScores.bind(this);
+  }
+
+  addScores(value) {
+    this.state.scores.push(value);
   }
 
   handlePinsEntryChange(enteredValue) {
+    //   if (enteredValue >= 0 && enteredValue <= 10) {
+    //     console.log(enteredValue);
+    //     this.state.scores.push(enteredValue);
+    //   }
     if (enteredValue >= 0 && enteredValue <= 10) {
-      this.setState({rollValue: enteredValue});
+      this.setState({rollValue: enteredValue}, () => this.addScores(this.state.rollValue));
     } else {
       alert('Please enter a number between 0 and 10');
     }
